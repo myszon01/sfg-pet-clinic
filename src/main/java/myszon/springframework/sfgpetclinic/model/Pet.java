46b5d8e391f287c3ce9df493b26f55911190907b
Peter Myszkowski <myszon01@gmail.com>
2018-12-19 13:11:45 -0800

    adding POJOs. Closes #2.

---
+++ src/main/java/myszon/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package myszon.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthdayDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthdayDate() {
+        return birthdayDate;
+    }
+
+    public void setBirthdayDate(LocalDate birthdayDate) {
+        this.birthdayDate = birthdayDate;
+    }
+}
