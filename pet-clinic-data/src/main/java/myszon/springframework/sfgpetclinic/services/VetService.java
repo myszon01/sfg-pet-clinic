c1014c07760dfe043ac38ec281f93d3c720f9d37
Peter Myszkowski <myszon01@gmail.com>
2018-12-22 00:32:57 -0800

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/myszon/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,16 @@
+package myszon.springframework.sfgpetclinic.services;
+
+import myszon.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findByLastName(String lastName);
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
