a8f996664f155af15f6f2a1d5a3d52aedee25588
Peter Myszkowski <myszon01@gmail.com>
2018-12-24 10:48:01 -0800

    Create base interface. Closes #11

--- pet-clinic-data/src/main/java/myszon/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/myszon/springframework/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package myszon.springframework.sfgpetclinic.services;
 
 import myszon.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CRUDService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
