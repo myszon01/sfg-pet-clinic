85c10834b0221df85749a77abfe6ef30061bffec
myszon01 <myszon01@gmail.com>
2019-01-23 15:12:26 -0800

    Added PetTypeService class. Closes #29

---
+++ pet-clinic-data/src/main/java/myszon/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package myszon.springframework.sfgpetclinic.services;
+
+import myszon.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CRUDService<PetType, Long> {
+}
