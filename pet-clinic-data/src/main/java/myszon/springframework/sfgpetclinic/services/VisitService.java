b61f59c8c640ce3ce892f288dc31a9d0c46b1caa
myszon01 <myszon01@gmail.com>
2019-01-29 22:19:56 -0800

    implemented Map impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/myszon/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package myszon.springframework.sfgpetclinic.services;
+
+import myszon.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CRUDService<Visit, Long> {
+}
